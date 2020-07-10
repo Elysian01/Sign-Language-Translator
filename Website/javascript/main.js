6dfcaadab823601da17a8da14e6c69c4484074b1
Abhishek Gupta <abhig0209@gmail.com>
2020-07-10 07:08:31 +0530

    Training page Backend and Modelling Completed

--- Website/javascript/main.js
+++ Website/javascript/main.js
@@ -1,7 +1,11 @@
-var classes = [] // list of classes
-
+var identity = 0;
+var classes = []; // list of classes
+// classes = [ {id:1 , name : "Hello" , count : 0}]
 
 const start = async () => {
+     const trainingCards = document.getElementById("training-cards")
+     const predictions = document.getElementById("predictions")
+     const confidence = document.getElementById("confidence")
 
      const createKNNClassifier = async () => {
           console.log('Loading KNN Classifier');
@@ -21,25 +25,38 @@ const start = async () => {
      const knnClassifierModel = await createKNNClassifier();
      const webcamInput = await createWebcamInput();
 
+     const addClass = () => {
+          // const inputClassName = document.getElementById("inputClassName").value
+
+          Classname = inputClassName.value
+          const found = classes.some(el => el.name === Classname);
+          if (!found) {
+               identity += 1
+               classes.push({ id: identity, name: Classname, count: 0 });
+          }
+
+
+          trainingCards.innerHTML += '<div class="grey-bg"><div class="text-center"><h3>Class Name : <span>' + Classname + '</span></h3><h3>Images : <span id = "images-' + identity + '" >0</span></h3></div ><div><button class="dark btn-spread btn-shadow mr-5" id="' + identity + '">Add New Images <i class="fas fa-plus fa-1x"></i></button></div></div>'
+
+          document.getElementById(identity.toString()).addEventListener('click', () => addDatasetClass(identity));
+          inputClassName.value = ""
+          console.log(classes)
+     }
+
 
      const initializeElements = () => {
-          document.getElementById('class-a').addEventListener('click', () => addDatasetClass(0));
-          document.getElementById('class-b').addEventListener('click', () => addDatasetClass(1));
-          document.getElementById('class-c').addEventListener('click', () => addDatasetClass(2));
+          const inputClassName = document.getElementById("inputClassName").value
+          document.getElementById('add-button').addEventListener('click', () => addClass(inputClassName));
+
+          document.getElementById('save_button').addEventListener('click', async () => downloadModel(knnClassifierModel));
 
      };
 
      const addDatasetClass = async (classId) => {
+
           // Capture an image from the web camera.
           const img = await webcamInput.capture();
 
-          const found = classes.some(el => el.id === classId);
-          if (!found) classes.push({ id: classId, number: 0 });
-          let classIndex = classes.findIndex(el => el.id === classId)
-          currentCount = classes[classIndex].number
-          currentCount += 1
-          classes[classIndex].number = currentCount
-          console.log(classes)
           // Get the intermediate activation of MobileNet 'conv_preds' and pass that
           // to the KNN classifier.
           const activation = mobilenetModel.infer(img, 'conv_preds');
@@ -47,8 +64,38 @@ const start = async () => {
           // Pass the intermediate activation to the classifier.
           knnClassifierModel.addExample(activation, classId);
 
+          let classIndex = classes.findIndex(el => el.id === classId)
+          currentCount = classes[classIndex].count
+          currentCount += 1
+          classes[classIndex].count = currentCount
+
+          var temp_id = 'images-' + classId.toString()
+          document.getElementById(temp_id).innerHTML = currentCount;
+
           // Dispose the tensor to release the memory.
           img.dispose();
+     };
+
+
+     const saveClassifier = async (classifierModel) => {
+          let datasets = await classifierModel.getClassifierDataset();
+          let datasetObject = {};
+          Object.keys(datasets).forEach(async (key) => {
+               let data = await datasets[key].dataSync();
+               datasetObject[key] = Array.from(data);
+          });
+          let jsonModel = JSON.stringify(datasetObject);
+
+          let downloader = document.createElement('a');
+          downloader.download = "model.json";
+          downloader.href = 'data:text/text;charset=utf-8,' + encodeURIComponent(jsonModel);
+          document.body.appendChild(downloader);
+          downloader.click();
+          downloader.remove();
+     };
+
+     const downloadModel = async (classifierModel) => {
+          saveClassifier(classifierModel);
      };
 
      const imageClassificationWithTransferLearningOnWebcam = async () => {
@@ -62,11 +109,16 @@ const start = async () => {
                     // Get the most likely class and confidences from the classifier module.
                     const result = await knnClassifierModel.predictClass(activation);
 
-                    const classes = ['A', 'B', 'C'];
-                    document.getElementById('console').innerText = `
-        prediction: ${classes[result.label]}\n
-        probability: ${result.confidences[result.label]}
-      `;
+                    console.log(classes[result.label - 1].name)
+                    predictions.innerHTML = classes[result.label - 1].name
+                    console.log(result.confidences[result.label])
+
+                    confidence.innerHTML = Math.floor(result.confidences[result.label] * 100)
+
+                    // const classes = ['A', 'B', 'C'];
+                    // document.getElementById('console').innerText = `
+                    // prediction: ${classes[result.label]}\n
+                    // probability: ${result.confidences[result.label]}`;
 
                     // Dispose the tensor to release the memory.
                     img.dispose();
@@ -74,6 +126,7 @@ const start = async () => {
                await tf.nextFrame();
           }
      };
+
      await initializeElements();
      await imageClassificationWithTransferLearningOnWebcam();
 };
