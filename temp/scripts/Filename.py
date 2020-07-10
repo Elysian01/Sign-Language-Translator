dd17a022ccfbbb91af639222f74c947bed53520a
Abhishek Gupta <abhig0209@gmail.com>
2020-07-11 03:12:32 +0530

    gifs Files renamed

---
+++ temp/scripts/Filename.py
@@ -0,0 +1,33 @@
+import os
+import re
+
+'''
+Function to rename gif files
+'''
+
+path = "D:\\IT\\Projects\\Sign-Language-Translator\\static\\ISL_Gifs\\"
+
+files = [0 for i in range(86)]
+
+
+def rename_gifs():
+    for count, filename in enumerate(os.listdir(path)):
+        print(filename)
+        print(count)
+
+        dst = path + str(count) + "-" + filename
+        src = path + filename
+        os.rename(src, dst)
+
+
+def create_dict_of_files():
+    for count, filename in enumerate(os.listdir(path)):
+        number = filename.split("-")[0]
+        files[int(number)] = filename
+    return files
+
+
+if __name__ == '__main__':
+    print(files)
+    print(create_dict_of_files())
+    #     rename_gifs()
