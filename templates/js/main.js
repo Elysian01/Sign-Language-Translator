a5c145b5946e3f6e2cfcefc5f75e323add0f2355
Abhishek Gupta <abhig0209@gmail.com>
2021-04-17 03:06:41 +0530

    text to speeh bug resolved

--- templates/js/main.js
+++ templates/js/main.js
@@ -175,41 +175,15 @@ const start = async() => {
     };
 
 
-    var voiceList = document.querySelector('#voiceList');
     var btnSpeak = document.querySelector('#btnSpeak');
-    var synth = window.speechSynthesis;
-    var voices = [];
-
-    PopulateVoices();
-    if (speechSynthesis !== undefined) {
-        speechSynthesis.onvoiceschanged = PopulateVoices;
-    }
 
     btnSpeak.addEventListener('click', () => {
-        var toSpeak = new SpeechSynthesisUtterance(text);
-        var selectedVoiceName = voiceList.selectedOptions[0].getAttribute('data-name');
-        voices.forEach((voice) => {
-            if (voice.name === selectedVoiceName) {
-                toSpeak.voice = voice;
-            }
-        });
-        synth.speak(toSpeak);
+        var msg = new SpeechSynthesisUtterance();
+        msg.text = predictions.innerHTML;
+        window.speechSynthesis.speak(msg);
     });
 
-    function PopulateVoices() {
-        voices = synth.getVoices();
-        var selectedIndex = voiceList.selectedIndex < 0 ? 0 : voiceList.selectedIndex;
-        voiceList.innerHTML = '';
-        voices.forEach((voice) => {
-            var listItem = document.createElement('option');
-            listItem.textContent = voice.name;
-            listItem.setAttribute('data-lang', voice.lang);
-            listItem.setAttribute('data-name', voice.name);
-            voiceList.appendChild(listItem);
-        });
 
-        voiceList.selectedIndex = selectedIndex;
-    }
 
     await initializeElements();
     await imageClassificationWithTransferLearningOnWebcam();
